4043e4a0e5d5fa899514a60d747633c5064c995c
mihiro <f.mihiroxx@gmail.com>
2019-09-02 13:58:34 +0900

    Add synonyms

---
+++ synonyms/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"commandline_tool_for_dmain_research/thesaurus"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("BTH_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalf("%qの類語検索に失敗しました: %v\n", word, err)
+		}
+		if len(syns) == 0 {
+			log.Fatalf("%qに類語はありませんでした\n")
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
